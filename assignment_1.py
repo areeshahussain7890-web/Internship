4681a2899a2aa2e0e6475c159b588e94b2118572
Areesha Hussain <TAyub@lmkr.com>
2026-04-29 20:56:46 +0500

    Assignment 1

--- assignment_1.py
+++ assignment_1.py
@@ -80,7 +80,6 @@ print("Intercept = ", model.intercept_)
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import warnings
